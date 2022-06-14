0254ec017294e494f014f93f2e1b395cf6c3ff36
Mehtab Akram <ostabi@yahoo.com>
2022-06-14 15:06:04 +0500

    Added Not found middleware.

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const express = require('express');
 const tasks = require('./routes/task')
 const connectDB = require('./db/connect')
 require('dotenv').config()
+const notFound = require('./middleware/not-found')
 const app = express();
 const port = 3000;
 app.use(express.static("./public"))
 app.use(express.json())
 app.use('/api/v1/tasks',tasks);
-
-
+app.use(notFound);
 
 
 
