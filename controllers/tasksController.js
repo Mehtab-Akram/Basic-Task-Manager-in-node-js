7946f39cd60f9bdd700545623baf4a744a54b495
Mehtab Akram <ostabi@yahoo.com>
2022-06-14 14:47:30 +0500

    Added Wrapper for async operations in controllers.

--- controllers/tasksController.js
+++ controllers/tasksController.js
@@ -1,47 +1,27 @@
 const mongoose = require('mongoose')
 const Task  = require('../models/task')
+const asyncWrapper = require('../middleware/async')
+const getAllTasks = asyncWrapper( async (req,res)=>{
+    const tasks = await Task.find({});
+    res.status(200).json({tasks});    
+})
 
-const getAllTasks = async (req,res)=>{
-    try {
-        const tasks = await Task.find({});
-        res.status(200).json({tasks});
-        
-    } catch (error) {
-        console.log(error)
-        res.status(500).json(error)
-        
-    }
-}
-
-const getTask = async (req,res)=>{
-    try {
+const getTask = asyncWrapper(async (req,res)=>{
         const {id:taskId} = req.params
         const task = await Task.findOne({_id : taskId})
         if(!task)
         {
-            res.status(404).json({msg:`Task not found with ID : ${taskId}`})
+            return res.status(404).json({msg:`Task not found with ID : ${taskId}`})
         }
         res.status(200).json({task});
-        
-    } catch (error) {
-        console.log(error)
-        res.status(500).json(error)
-        
-    }
-}
+})
 
-const createTask = async (req,res)=>{
-    try {
-        const task = await Task.create(req.body);
+const createTask = asyncWrapper( async (req,res)=>{
+     const task = await Task.create(req.body);
         res.status(201).json({task})
-    } catch (error) {
-        res.status(500).json({msg:`An error occured on the server side. Please try again later.`})
-    }
-    
-}
+})
 
-const updateTask = async (req,res)=>{
-    try {
+const updateTask = asyncWrapper( async (req,res)=>{
         const{id:taskId} = req.params
         const task = await Task.findOneAndUpdate({_id: taskId},req.body,{
             runValidators: true,
@@ -50,21 +30,14 @@ const updateTask = async (req,res)=>{
         if(!task){
             return res.status(404).json({msg: `Task not found against Id : ${taskId}`})
         }
-        res.status(201).json({task})
-    } catch (error) {
-        res.send(500).json({msg:`An error occured on the server side. Please try again later.`})
-    }
-}
+        res.status(201).json({task})  
+})
 
-const deleteTask = async (req,res)=>{
-    try {
+const deleteTask = asyncWrapper( async (req,res)=>{
         const{id:taskId} = req.params
         const task = await Task.findOneAndDelete({_id: taskId});
         res.status(201).json({task})
-    } catch (error) {
-        res.send(500).json({msg:`An error occured on the server side. Please try again later.`})
-    }
-}
+})
 
 
 module.exports = {
